b7f42ce60a3d0befa13f1d821fe3ad5847e2c3bd
MennaaMohamed <33002017+MennaaMohamed@users.noreply.github.com>
2018-11-20 23:03:36 +0200

    Connected to Firebase

--- app/src/main/java/com/example/sara/grammy/MainActivity.java
+++ app/src/main/java/com/example/sara/grammy/MainActivity.java
@@ -5,13 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import com.example.sara.grammy.Utils.BottomNavigationViewHelper;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavViewBar);
         BottomNavigationViewHelper.removeShiftMode(bottomNavigationView);
     }
