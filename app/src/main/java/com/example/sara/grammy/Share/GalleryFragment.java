4da534ee162ec244e805afe6da10e11e233ca673
MennaaMohamed <33002017+MennaaMohamed@users.noreply.github.com>
2018-12-26 22:58:14 +0200

    ShareActivity
    Reads directories and file paths, loads images and are selected and viewed in gridview

--- app/src/main/java/com/example/sara/grammy/Share/GalleryFragment.java
+++ app/src/main/java/com/example/sara/grammy/Share/GalleryFragment.java
@@ -1,11 +1,14 @@
 package com.example.sara.grammy.Share;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -13,16 +16,28 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.sara.grammy.R;
+import com.example.sara.grammy.Utils.FilePaths;
+import com.example.sara.grammy.Utils.FileSearch;
+import com.example.sara.grammy.Utils.GridImageAdapter;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class GalleryFragment extends android.support.v4.app.Fragment {
     private static final String TAG = "GalleryFragment";
+    private static final int NUM_GRID_COLUMNS = 3;
 
     private GridView gridView;
     private ImageView galleryImage;
     private ProgressBar mProgressBar;
     private Spinner directorySpinner;
+
+    private ArrayList<String> directories;
+    private String mAppend = "file:/";
 
     @Nullable
     @Override
@@ -33,6 +48,7 @@ public class GalleryFragment extends android.support.v4.app.Fragment {
         directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
         mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
         mProgressBar.setVisibility(View.GONE);
+        directories = new ArrayList<>();
         Log.d(TAG, "onCreateView: started.");
 
         ImageView closeShare = (ImageView) view.findViewById(R.id.ivcloseShare);
@@ -52,7 +68,88 @@ public class GalleryFragment extends android.support.v4.app.Fragment {
                 Log.d(TAG, "onClick: navigating to the final share screen");
             }
         });
+        init();
 
         return view;
     }
+
+    private void init(){
+        FilePaths filePaths = new FilePaths();
+        if(FileSearch.getDirectoryPaths(filePaths.PICTURES) != null){
+            directories = FileSearch.getDirectoryPaths(filePaths.PICTURES);
+        }
+        directories.add(filePaths.CAMERA);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, directories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        directorySpinner.setAdapter(adapter);
+
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemSelected: selected: " + directories.get(position));
+                setupGridView(directories.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void setupGridView(String selectedDirectory){
+        Log.d(TAG, "setupGridView: directory chosen: " + selectedDirectory);
+        final ArrayList<String> imgURLs = FileSearch.getFilePaths(selectedDirectory);
+
+        //set grid column width
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imgWidth = gridWidth/NUM_GRID_COLUMNS;
+        gridView.setColumnWidth(imgWidth);
+
+        //use the grid adapter to adapt images to gridview
+        GridImageAdapter adapter = new GridImageAdapter(getActivity(), R.layout.layout_grid_imageview, mAppend, imgURLs);
+        gridView.setAdapter(adapter);
+
+        setImage(imgURLs.get(0), galleryImage, mAppend);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected an image: " + imgURLs.get(position));
+                setImage(imgURLs.get(position), galleryImage, mAppend);
+            }
+        });
+
+    }
+
+    private void setImage(String imgURL, ImageView image, String append){
+
+        Log.d(TAG, "setImage: setting image");
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+
+            }
+        });
+    }
+
+
 }
