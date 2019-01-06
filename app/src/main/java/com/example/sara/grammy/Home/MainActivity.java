8b47e196b46890698022aff6339559258ae8147f
Sara Hassan <saraaawwad12@gmail.com>
2019-01-07 01:18:00 +0200

    merge2

--- app/src/main/java/com/example/sara/grammy/Home/MainActivity.java
+++ app/src/main/java/com/example/sara/grammy/Home/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "new nav red");
+        Log.d(TAG, "new after pull");
         setupFirebaseAuth();
         initImageLoader();
         setUpBottomNav();
