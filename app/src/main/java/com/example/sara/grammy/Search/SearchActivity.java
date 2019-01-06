a6fb74c8867e954cc7000bbdedf733c226c33401
Farah Hisham Ahmed Mohamed Soliman <farah1502451@miuegypt.edu.eg>
2019-01-06 18:38:59 +0200

    File Paths

--- app/src/main/java/com/example/sara/grammy/Search/SearchActivity.java
+++ app/src/main/java/com/example/sara/grammy/Search/SearchActivity.java
@@ -4,28 +4,137 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.example.sara.grammy.R;
 import com.example.sara.grammy.Utils.BottomNavigationViewHelper;
+import com.example.sara.grammy.Utils.UserListAdapter;
+import com.example.sara.grammy.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
     private static final String TAG = "SearchActivity";
     private Context mContext = SearchActivity.this;
     private static final int ACTIVITY_NUM = 1;
 
+    //widgets
+    private EditText mSearchParam;
+    private ListView mListView;
+
+    //vars
+    private List<User> mUserList;
+    private UserListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate:Started Search");
+        mSearchParam = (EditText) findViewById(R.id.search);
+        mListView = (ListView) findViewById(R.id.listView);
 
+        hideSoftKeyboard();
         setUpBottomNav();
+        initTextListener();
 
+    }
+
+    private void initTextListener(){
+        mUserList = new ArrayList<>();
+
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = mSearchParam.getText().toString().toLowerCase(Locale.getDefault());
+                searchForMatch(text);
+            }
+        });
+    }
+
+
+    private void searchForMatch(String keyword){
+
+        mUserList.clear();
+
+        //update the users list view
+        if(keyword.length() ==0){
+
+        }else{
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username)).equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                        mUserList.add(singleSnapshot.getValue(User.class));
+                        //update the users list view
+                        //updateUsersList();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+
+    private void updateUsersList(){
+        mAdapter = new UserListAdapter(SearchActivity.this, R.layout.layout_user_listitem, mUserList);
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: " + mUserList.get(position).toString());
+
+                //navigate to profile activity
+//                Intent intent =  new Intent(SearchActivity.this, ProfileActivity.class);
+//                intent.putExtra(getString(R.string.calling_activity), getString(R.string.search_activity));
+//                intent.putExtra(getString(R.string.intent_user), mUserList.get(position));
+//                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    private void hideSoftKeyboard(){
+        if(getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
     public void setUpBottomNav(){
--- app/src/main/java/com/example/sara/grammy/Utils/FilePaths.java
+++ app/src/main/java/com/example/sara/grammy/Utils/FilePaths.java
@@ -3,7 +3,6 @@ package com.example.sara.grammy.Utils;
 import android.os.Environment;
 
 public class FilePaths {
-
     public String ROOT_DIR = Environment.getExternalStorageDirectory().getPath();
     public String PICTURES = ROOT_DIR + "/Pictures";
     public String CAMERA = ROOT_DIR + "/DCIM/Camera";
